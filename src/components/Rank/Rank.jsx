24bf2f8d4bc60ad7a8655fc8a0f613c6cc5f8ae0
CharlesChan <charleschanlok@gmail.com>
2019-02-12 11:37:58 +0800

    added rank function with aws lambda

--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -1,15 +1,50 @@
 import React from 'react';
 
-const Rank = (props) => {
-    const { name, entries } = props;
+class Rank extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emoji: ''
+    };
+  }
+
+  componentDidMount() {
+    this.generateEmoji(this.props.entries);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.entries === this.props.entries && prevProps.name === this.props.name) {
+      return;
+    } else {
+      this.generateEmoji(this.props.entries);
+    }
+  }
+
+  generateEmoji = async (entries) => {
+    try {
+      const res = await fetch(
+        `${process.env.REACT_APP_AWS_LAMBDA_RANK_API}?rank=${entries}`
+      );
+      const data = await res.json();
+      this.setState({ emoji: data.input });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  render() {
     return (
-        <div >
-            {`${name} , you uploaded`}
-            <div className='white f1 '>
-                {`${entries} photos.`}
-            </div>
+      <div>
+        {`${this.props.name} , you uploaded`}
+        <div className="white f1">
+          {`${this.props.entries} photos.`}
         </div>
-    )
-};
+        <div className="white f3">
+          {`Rank Badge: ${this.state.emoji}`}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Rank;
